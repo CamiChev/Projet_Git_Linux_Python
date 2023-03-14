8f3a793d08f46aa7530f5036c7a0b09e626357e4
CamiChev <camille.chevillon@edu.devinci.com>
2023-03-14 12:51:52 +0100

    initial commit

---
+++ graphic.py
@@ -0,0 +1,34 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+
+# Charger les donnees CSV
+df = pd.read_csv('prices.txt', sep = ',', names = ['prix', 'temps'])
+df['prix'] = df['prix'].str.replace('$', '')
+df['prix'] = df['prix'].str.replace(' ', '').astype('float')
+
+# Creer l'application Dash
+app = dash.Dash(__name__)
+
+# Creer la mise en page de l'application
+app.layout = html.Div(children=[
+    html.H1(children='Evolution du prix'),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df['temps'], 'y': df['prix'], 'type': 'line', 'name': 'Donnees'},
+            ],
+            'layout': {
+                'title': 'Evolution du prix'
+            }
+        }
+    )
+])
+
+# Executer l'application
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ script.sh
@@ -0,0 +1,14 @@
+#Telechargement des donnees
+curl -s https://coinpaprika.com/coin/wbtc-wrapped-bitcoin/ -o /home/camche/Projet/file.html
+#Visualiser
+#more file.html
+
+
+#Recuperer prix et creation nouveau dossier (on cherche le prix a la ligne "coinPrice" et on cherche la ligne juste apres pour avoir le prix seulement)
+price=$(grep "coinPrice" /home/camche/Projet/file.html -A1 | tail -1)
+
+#Affichage date et temps
+date="$(date +"%Y-%m-%d %H:%M:%S")"
+
+echo "$price, $date" >> /home/camche/Projet/prices.txt
+
